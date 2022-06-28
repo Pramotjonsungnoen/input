424f33bd6b6530a448a5214641f6d2c2d6355396
Pramotjonsungnoen <92082350+Pramotjonsungnoen@users.noreply.github.com>
2022-06-28 10:18:04 +0700

    Update blocks.js

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -17,21 +17,21 @@ Blockly.defineBlocksWithJsonArray([
       "name": "sort"
     }*/
  // ]
-  "type": "analog_read",
-  "message0": "pin %1 repeat: %2 sort: %3 ",
-  "args0": [
-      {
-        "type": "input_value",
-        "name": "trig"
-      },
-      {
-        "type": "input_value",
-        "name": "repeat"
-      },
-      {
-        "type": "input_value",
-        "name": "sort"
-      }
+ "type": "oled_draw_text",
+ "message0": "OLED draw text %1 at (x: %2 , y: %3 )",
+ "args0": [
+   {
+     "type": "input_value",
+     "name": "trig"
+   },
+   {
+     "type": "input_value",
+     "name": "ANS"
+   },
+   {
+     "type": "input_value",
+     "name": "sort"
+   }
   ],
   "output": "Number",
   "colour": "#004098",
